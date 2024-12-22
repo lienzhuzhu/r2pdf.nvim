c70a9f48d540b94c077cfdd0040b04260f362e0a
Lien Zhu <95384975+lienzhuzhu@users.noreply.github.com>
2024-12-22 04:10:25 -0600

    removed indent autocmd

--- lua/r2pdf/commands.lua
+++ lua/r2pdf/commands.lua
@@ -5,12 +5,3 @@
 
 -- User command to render_pdf()
 vim.api.nvim_create_user_command("R2PDF", require("r2pdf.rendering").render_pdf, {})
-
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "rmd",
-    callback = function()
-        vim.opt_local.formatoptions = ""
-        vim.opt_local.autoindent = false        -- Disable auto-indentation
-        vim.opt_local.smartindent = false       -- Disable smart indentation
-    end,
-})
